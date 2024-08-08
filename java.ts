73040e31481a39cb97a8f7b5ebd3d32bde9d75fa
Icedex231 <101020915+Icedex231@users.noreply.github.com>
2024-08-09 02:47:18 +0800

    Add files via upload

---
+++ java.ts
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Happy Birthday po! I love you so much and thank you for being my girlfriend I'm really lucky for your choosing me ");
+});
